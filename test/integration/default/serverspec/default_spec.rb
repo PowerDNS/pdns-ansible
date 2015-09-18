29bf1ce04bd2b9132fd412832c3f86f4679a6d43
Pieter Lexis <pieter.lexis@powerdns.com>
2015-09-18 16:48:33 +0200

    Add more tests, some of which are broken at the moment

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,9 +3,30 @@ require 'serverspec'
 # Required by serverspec
 set :backend, :exec
 
-describe "PowerDNS" do
-  describe user('pdns') do
-    it { should exist }
-    it { should belong_to_group('pdns') }
-  end
+describe user('pdns') do
+  it { should exist }
+  it { should belong_to_group('pdns') }
+end
+
+describe service('pdns') do
+  it { should be_enabled }
+  it { should be_running }
+end
+
+describe file('/etc/powerdns/pdns.conf') do
+  it { should be_file }
+  it { should be_owned_by 'root' }
+  it { should be_grouped_into 'root' }
+end
+
+describe port(53) do
+  it { should be_listening.with('udp') }
+  it { should be_listening.with('udp6') }
+  it { should be_listening.with('tcp') }
+  it { should be_listening.with('tcp6') }
+end
+
+describe process('pdns_server') do
+  its(:user) { should eq "pdns" }
+  its(:group) { should eq "pdns" }
 end
