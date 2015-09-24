3365e7c99828d1644cc267f83511bda8543dca73
Pieter Lexis <pieter.lexis@powerdns.com>
2015-09-24 13:16:09 +0200

    Use amazon for tests
    * Add tests for repo.powerdns.com
    * Fix tasks that failed (yay tests!)

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,7 +1,4 @@
-require 'serverspec'
-
-# Required by serverspec
-set :backend, :exec
+require 'spec_helper'
 
 describe user('pdns') do
   it { should exist }
@@ -13,18 +10,24 @@ describe service('pdns') do
   it { should be_running }
 end
 
+describe port(53) do
+  it { should be_listening.with('udp') }
+  it { should be_listening.with('tcp') }
+end
+
 describe file('/etc/powerdns/pdns.conf') do
   it { should be_file }
   it { should be_owned_by 'root' }
   it { should be_grouped_into 'root' }
 end
 
-describe port(53) do
-  it { should be_listening.with('udp') }
-  it { should be_listening.with('tcp') }
-end
-
-describe process('pdns_server') do
+describe process('pdns_server'), :if => os[:family] != 'ubuntu' do
   its(:user) { should eq "pdns" }
   its(:group) { should eq "pdns" }
+end
+
+# Because we run inside a guardian, there are 2 processes in ubuntu, but we
+# only see the first one
+describe command('ps --user pdns -o user=,group=,cmd='), :if => os[:family] == 'ubuntu' do
+  its(:stdout) { should match /pdns     pdns     \/usr\/sbin\/pdns_server-instance --daemon --guardian=yes/ }
 end
