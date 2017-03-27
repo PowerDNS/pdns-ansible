c0f9a6a679a589c433f32cd97378a1f2d1021c41
Alain ROMEYER <aromeyer@vente-privee.com>
2017-03-27 17:13:55 +0200

    used variable pdns_config_dir_user instaead of full path + remove default launch directive from user configuration file + add some tests according to new configuration file construction

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -25,3 +25,41 @@ def test_database_exists(File):
     assert f.group == 'pdns'
     assert f.mode == 420
     assert f.size > 10000
+
+def test_config(File, SystemInfo, Sudo):
+    with Sudo():
+        f = None
+        if SystemInfo.distribution in debian_os:
+            f = File('/etc/powerdns/pdns.conf')
+        if SystemInfo.distribution in rhel_os:
+            f = File('/etc/pdns/pdns.conf')
+            
+        assert f.exists
+        assert 'launch=' in f.content
+            
+def test_config_user(File, SystemInfo, Sudo):
+    with Sudo():
+        f = None
+        if SystemInfo.distribution in debian_os:
+            f = File('/etc/powerdns/pdns.d/pdns.local.conf')
+        if SystemInfo.distribution in rhel_os:
+            f = File('/etc/pdns//pdns.d/pdns.local.conf')
+            
+        assert f.exists
+        assert 'config-dir=' in f.content
+        assert 'setuid=' in f.content
+        assert 'setgid=' in f.content
+        assert ! 'launch=' in f.content
+
+def test_config_sqlite(File, SystemInfo, Sudo):
+    with Sudo():
+        f = None
+        if SystemInfo.distribution in debian_os:
+            f = File('/etc/powerdns/pdns.d/pdns.local.sqlite.conf')
+        if SystemInfo.distribution in rhel_os:
+            f = File('/etc/pdns/pdns.d/pdns.local.sqlite.conf')
+            
+        assert f.exists
+        assert 'launch+=gsqlite' in f.content
+        assert 'gsqlite3-database=' in f.content
+        
