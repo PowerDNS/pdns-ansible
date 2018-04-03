7d9e6a5e0741973b13a4d81c52c1176be7117d18
Andrea Tosatto <andrea.tosy@gmail.com>
2018-04-03 10:54:37 +0200

    Tests the MySQL and SQLite backends in the same scenario

--- molecule/resources/tests/all/test_common.py
+++ molecule/resources/tests/all/test_common.py
@@ -7,6 +7,17 @@ def test_distribution(host):
     assert host.system_info.distribution.lower() in debian_os + rhel_os
 
 
+def test_repo_pinning_file(host):
+    if host.system_info.distribution.lower() in debian_os:
+        f = host.file('/etc/apt/preferences.d/pdns')
+        assert f.exists
+        assert f.user == 'root'
+        assert f.group == 'root'
+        f.contains('Package: pdns-*')
+        f.contains('Pin: origin repo.powerdns.com')
+        f.contains('Pin-Priority: 600')
+
+
 def test_package(host):
     p = None
     if host.system_info.distribution.lower() in debian_os:
