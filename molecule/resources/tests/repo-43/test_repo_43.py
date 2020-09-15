4110a4896a722dca26fd198a4d1666630813d8c5
Pieter Lexis <pieter.lexis@powerdns.com>
2020-09-15 16:34:58 +0200

    Add 4.3 tests

---
+++ molecule/resources/tests/repo-43/test_repo_43.py
@@ -0,0 +1,33 @@
+
+debian_os = ['debian', 'ubuntu']
+rhel_os = ['redhat', 'centos']
+
+
+def test_repo_file(host):
+    f = None
+    if host.system_info.distribution.lower() in debian_os:
+        f = host.file('/etc/apt/sources.list.d/powerdns-auth-43.list')
+    if host.system_info.distribution.lower() in rhel_os:
+        f = host.file('/etc/yum.repos.d/powerdns-auth-43.repo')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_pdns_repo(host):
+    f = None
+    if host.system_info.distribution.lower() in debian_os:
+        f = host.file('/etc/apt/sources.list.d/powerdns-auth-43.list')
+    if host.system_info.distribution.lower() in rhel_os:
+        f = host.file('/etc/yum.repos.d/powerdns-auth-43.repo')
+
+    assert f.exists
+    assert f.contains('auth-43')
+
+
+def test_pdns_version(host):
+    cmd = host.run('/usr/sbin/pdns_server --version')
+
+    assert 'PowerDNS Authoritative Server' in cmd.stderr
+    assert '4.3' in cmd.stderr
