17a9b3d93879686545fff156f9dbe8b2b08a1737
Pieter Lexis <pieter.lexis@powerdns.com>
2020-09-17 12:16:23 +0200

    Add archlinux tests

--- molecule/resources/tests/backend-sqlite/test_backend_sqlite.py
+++ molecule/resources/tests/backend-sqlite/test_backend_sqlite.py
@@ -1,23 +1,27 @@
 
 debian_os = ['debian', 'ubuntu']
 rhel_os = ['redhat', 'centos']
+archlinux_os = ['arch']
 
 
 def test_package(host):
-    p = None
-    if host.system_info.distribution.lower() in debian_os:
-        p = host.package('pdns-backend-sqlite3')
-    if host.system_info.distribution.lower() in rhel_os:
-        p = host.package('pdns-backend-sqlite')
+    if host.system_info.distribution.lower() in debian_os + rhel_os:
+        if host.system_info.distribution.lower() in debian_os:
+            p = host.package('pdns-backend-sqlite3')
+        if host.system_info.distribution.lower() in rhel_os:
+            p = host.package('pdns-backend-sqlite')
 
-    assert p.is_installed
+        assert p.is_installed
 
 
 def test_database_exists(host):
     f = host.file('/var/lib/powerdns/pdns.db')
+    user = 'pdns'
+    if host.system_info.distribution.lower() in archlinux_os:
+        user = 'powerdns'
 
     assert f.exists
-    assert f.user == 'pdns'
-    assert f.group == 'pdns'
+    assert f.user == user
+    assert f.group == user
     assert f.mode == 0o640
     assert f.size > 10000
--- molecule/resources/tests/repo-master/test_repo_master.py
+++ molecule/resources/tests/repo-master/test_repo_master.py
@@ -26,6 +26,17 @@ def test_pdns_repo(host):
     assert f.contains('auth-master')
 
 
+def test_repo_pinning_file(host):
+    if host.system_info.distribution.lower() in debian_os:
+        f = host.file('/etc/apt/preferences.d/pdns')
+        assert f.exists
+        assert f.user == 'root'
+        assert f.group == 'root'
+        f.contains('Package: pdns-*')
+        f.contains('Pin: origin repo.powerdns.com')
+        f.contains('Pin-Priority: 600')
+
+
 def test_pdns_version(host):
     cmd = host.run('/usr/sbin/pdns_server --version')
 
